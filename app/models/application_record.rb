6309494441607ba0e46b64abe3239cc0545f2594
Austin Harshberger <aharshbe@live.com>
2018-07-31 16:03:26 -0700

    adding branch for testing with globals

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,5 +4,10 @@ class ApplicationRecord < ActiveRecord::Base
      # Creates global variables to store max scores for each creative quality
        $g_pmax = 0 
        $g_cmax = 0 
-       $g_emax = 0 
+       $g_emax = 0
+       
+ 	# Creates global variables to store raw scores for each creative quality
+       $g_preal = 0 
+       $g_creal = 0 
+       $g_ereal = 0 
 end
