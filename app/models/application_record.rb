26dfef78367e4f0d4ea9598cb2584a5999f446f7
Austin Harshberger <aharshbe@live.com>
2018-07-31 16:55:57 -0700

    adding comments/to-dos

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -5,9 +5,11 @@ class ApplicationRecord < ActiveRecord::Base
        $g_pmax = 0 
        $g_cmax = 0 
        $g_emax = 0
-       
+
  	# Creates global variables to store raw scores for each creative quality
        $g_preal = 0 
        $g_creal = 0 
        $g_ereal = 0 
+
+    # To do, seed data in global variables to prevent divide by 0 error
 end
