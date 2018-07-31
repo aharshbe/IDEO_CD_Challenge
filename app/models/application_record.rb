5ad37ed6407dfc8df7a52d2df5d81d6e69444733
Austin Harshberger <aharshbe@live.com>
2018-07-31 14:52:50 -0700

    moved the init for the globals to the application_record so they retain values

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+     # Creates global variables to store max scores for each creative quality
+       $g_pmax = 0 
+       $g_cmax = 0 
+       $g_emax = 0 
 end
